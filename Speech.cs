e468809b1557571d6c9303729b16a647dbd98d7c
Joshmens0 <154364963+Joshmens0@users.noreply.github.com>
2024-05-01 17:00:11 +0000

    2.01

---
+++ Speech.cs
@@ -0,0 +1,28 @@
+﻿using Cure_WPF;
+using OpenAI_API;
+using OpenAI_API.Audio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace Cure_2._0
+{
+    internal class Speech:Openai
+    {
+        public string Text { get; set; }
+        public async Task GetSpeech() 
+        {
+            var location = new Uri(@"/audio.wav");
+            var makeFile=File.Create(location.ToString());
+            TextToSpeechRequest request = new TextToSpeechRequest() { Voice="alloy",Input=Text};
+            var audioFile =await openai.TextToSpeech.SaveSpeechToFileAsync(request,makeFile.Name);
+            audioFile.Create();
+            
+        }
+    }
+}
---
+++ openai.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media.Animation;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Images;
+using OpenAI_API.Models;
+namespace Cure_WPF
+{
+    internal class Openai
+    {
+        public static string apikey = "sk-proj-bPKdFDTZEuH3PEIp7BvcT3BlbkFJYKHBaYQfbnqCdaiMu7XG";
+        public static OpenAIAPI openai = new OpenAIAPI(apikey);
+        public static OpenAI_API.Chat.Conversation chatgpt = openai.Chat.CreateConversation();
+        public string SystemMessage { get; set; }
+        public string UserRequest {  get; set; }
+        public async Task<string> MakeRequest()
+        {
+            chatgpt.Model=Model.ChatGPTTurbo_1106;
+            chatgpt.AppendSystemMessage(SystemMessage);
+            chatgpt.AppendUserInput(UserRequest);
+            var respond=await chatgpt.GetResponseFromChatbotAsync();
+            return respond;
+        }
+        
+       
+    }
+}
